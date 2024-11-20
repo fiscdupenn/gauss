f7cc8940419fe843e34f42d22f3d7cf55c047eaf
David Fischer <fiscd@seas.upenn.edu>
2024-11-20 10:19:09 -0500

    commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-
+'use client';
 
 import React from "react";
+import RangeSlider from "./components/RangeSlider";
 
   export default function Home() {
     return (
       <div>
         <h1>My Slider</h1>
+        {RangeSlider}
       </div>
     );
   }
